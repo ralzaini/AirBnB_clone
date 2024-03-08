08f31245e90be355c2400dcf870941899eeafc1a
ralzaini <rehabalzaini@gmail.com>
2024-03-08 14:18:10 +0300

    task 6

---
+++ console.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+    Module for the console
+"""
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Console class for HBNB project
+    """
+    prompt = "(hbnb)"
+
+    def do_exit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+    
+    def help_exit(self, arg):
+        """
+        Print help message for exit command
+        """
+        print("Quit command to exit the program")
+    
+    def do_EOF(self, arg):
+        """
+        Handle End-of-File (EOF) input
+        """
+        print("")
+        return True
+    
+    def emptyline(self):
+        """
+        Empty the last command
+        """
+        pass
+    
+if __name__ == "__main__":
+    HBNBCommand().cmdloop
