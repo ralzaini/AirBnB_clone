602a221e79f6f42c67c0b42194324369da7d5fd2
jawkat <kad.joliano@gmail.com>
2024-03-09 15:39:29 +0100

    fixing errors not working

--- models/base_model.py
+++ models/base_model.py
@@ -30,22 +30,18 @@ class BaseModel:
         updated_at: The same as created_at, initially set to the same value.
         """
         timef = "%Y-%m-%dT%H:%M:%S.%f"
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         setattr(self, key, datetime.strptime(value, timef))
-                    elif key == "id":
-                        setattr(self,key, str(value))
                     else:
                         setattr(self, key, value)
 
-
+        models.storage.new(self)
 
     def save(self):
         """
@@ -59,14 +55,12 @@ class BaseModel:
         """
         Returns a dictionary representation of the instance.
         """
+
         base_dict = self.__dict__.copy()
-
-
-        for key, value in base_dict.items():
-            if key in ["created_at", "updated_at"]:
-                base_dict[key] = value.isoformat()
-
         base_dict["__class__"] = self.__class__.__name__
+        base_dict["created_at"] = self.created_at.isoformat()
+        base_dict["updated_at"] = self.updated_at.isoformat()
+
         return base_dict
 
     def __str__(self) -> str:
