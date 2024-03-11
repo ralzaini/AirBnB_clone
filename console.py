ac12830e2856762c7b61fe9f295b36ce88305cca
ralzaini <rehabalzaini@gmail.com>
2024-03-11 14:57:34 +0300

    task 10 import

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
 
 
 class HBNBCommand(cmd.Cmd):
