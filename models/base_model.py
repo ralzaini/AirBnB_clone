67560b01aa93ebc72cf19b2808ecd1c0bafc916c
jawkat <kad.joliano@gmail.com>
2024-03-11 09:00:41 +0000

    try to solve taqsk 5

--- models/base_model.py
+++ models/base_model.py
@@ -61,8 +61,8 @@ class BaseModel:
 
         base_dict = self.__dict__.copy()
         base_dict["__class__"] = self.__class__.__name__
-        base_dict["created_at"] = self.created_at.strftime(TIMEF)[:-3]
-        base_dict["updated_at"] = self.updated_at.strftime(TIMEF)[:-3]
+        base_dict["created_at"] = self.created_at.isoformat()
+        base_dict["updated_at"] = self.updated_at.isoformat()
 
         return base_dict
 
